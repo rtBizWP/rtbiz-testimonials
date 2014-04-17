ee81fcb140f3f8a8ae5403dcad71ad9243bb1c73
Faishal Saiyed <saiyedfaishal@gmail.com>
2014-04-17 17:28:18 +0530

    Add Plugin index file

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Plugin Name: rtBiz Portfolio and Testimonials
+  Plugin URI: http://rtcamp.com/
+  Description: This plugin add Portfolio and Testimonial support
+  Version: 0.1-beta
+  Author: rtcamp
+  Author URI: http://rtcamp.com/
+ */
