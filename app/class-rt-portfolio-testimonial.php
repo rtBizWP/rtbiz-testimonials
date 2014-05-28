fd3ee971a55418614aff563dd23baf00a1e3e22c
Faishal Saiyed <saiyedfaishal@gmail.com>
2014-05-28 17:19:01 +0530

    Fix testimonial

---
+++ app/class-rt-portfolio-testimonial.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: faishal
+ * Date: 18/04/14
+ * Time: 6:57 PM
+ */
+class RT_Biz_Testimonial {
+	function __construct() {
+		add_action( 'plugins_loaded', 'plugins_loaded' );
+		add_action( 'init', array( &$this, 'init_0' ), 0 );
+		add_filter( 'rt_biz_modules', array( $this, 'register_rt_portfolio_testimonial_module' ) );
+		add_action( 'activated_plugin', 'flush_rewrite_rules' );
+		add_action( 'deactivate_plugin', 'flush_rewrite_rules' );
+		add_action( 'after_switch_theme', 'flush_rewrite_rules' );
+	}
+
+	function register_rt_portfolio_testimonial_module( $modules ) {
+		global $rt_wiki_roles;
+		$module_key = ( function_exists( 'rt_biz_sanitize_module_key' ) ) ? rt_biz_sanitize_module_key( RT_TESTIMONIAL ) : '';
+		$modules[ $module_key ] = array(
+			'label' => __( 'Portfolio & Testimonial' ),
+			'post_types' => array(
+				'portfolio',
+				'testimonial',
+			),
+		);
+		return $modules;
+	}
+
+	function plugins_loaded(){
+
+	}
+
+	function init_0() {
+		$this->register_custom_post_type();
+		$this->register_p2p_connections();
+	}
+
+	function register_p2p_connections(){
+		if ( function_exists( 'p2p_register_connection_type' ) ) {
+
+			p2p_register_connection_type(
+				array(
+					'name'        => 'testimonial_' . rt_biz_get_person_post_type(),
+					'to'          => rt_biz_get_person_post_type(),
+					'from'        => 'testimonial',
+					'cardinality' => 'one-to-one',
+					'title'       => array(
+						'to'   => 'Testimonial',
+						'from' => 'Client',
+					)
+				)
+			);
+		}
+
+	}
+
+
+	function register_custom_post_type() {
+		register_post_type(
+			'testimonial',
+			array(
+				'label'           => __( 'Testimonial' ),
+				'description'     => __( 'Manage Testimonial items easily' ),
+				'public'          => true,
+				'show_ui'         => true,
+				'taxonomies'      => array( 'post_tag' ),
+				'capability_type' => 'testimonial',
+				'supports'        => array(
+					'title',
+					'editor',
+					'excerpt',
+					'thumbnail',
+					'custom-fields',
+					'revisions',
+				),
+				'rewrite'     => array(
+					'slug'       => 'testimonials',
+					'with_front' => false,
+				),
+				'has_archive' => true,
+			)
+		);
+		add_rewrite_rule( '^testimonials/tag/([^/]*)/([^/]*)/([^/]*)/?','index.php?post_type=testimonial&tag=$matches[1]&paged=$matches[3]', 'top' );
+		add_rewrite_rule( '^testimonials/tag/([^/]*)/?','index.php?post_type=testimonial&tag=$matches[1]', 'top' );
+	}
+} 
--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 /*
-  Plugin Name: rtBiz Portfolio and Testimonials
-  Plugin URI: http://rtcamp.com/
-  Description: This plugin add Portfolio and Testimonial support
-  Version: 0.1-beta
-  Author: rtcamp
-  Author URI: http://rtcamp.com/
- */
+Plugin Name: rtBiz Portfolio and Testimonials
+Plugin URI: http://rtcamp.com/
+Description: This plugin add Portfolio and Testimonial support
+Version: 0.1-beta
+Author: rtcamp
+Author URI: http://rtcamp.com/
+*/
+
+define( 'RT_TESTIMONIAL', 'testimonial' );
+
+add_action( 'rt_biz_init', 'rt_biz_testimonial_init', 1 );
+
+function rt_biz_testimonial_init(){
+	$rt_biz_portfolio_testimonial_loader = new RT_WP_Autoload( trailingslashit( dirname( __FILE__ ) ) . 'app' );
+
+	$rt_biz_init = new RT_Biz_Testimonial();
+}
